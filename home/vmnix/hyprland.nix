74ca250d01fab28b469f94bf376a16f2f70b7969
cozyGalvinism <jean@der-capta.in>
2025-07-01 12:32:49 +0200

    Add hyprland binds

--- home/vmnix/hyprland.nix
+++ home/vmnix/hyprland.nix
@@ -5,11 +5,23 @@
     enable = true;
     package = null;
     portalPackage = null;
-    
+
     systemd.enable = true;
 
     settings = {
       "$mod" = "SUPER";
+
+      bind = []
+      ++ (
+        builtins.concatLists (builtins.genList (i:
+          let ws = i + 1;
+          in [
+            "$mod, code:1${toString i}, workspace, ${toString ws}"
+            "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+          ]
+        )
+        9)
+      );
     };
   };
 }
