a8a98de563b775ecd36aa28a51cd0a6df6cc1b64
cozyGalvinism <jean@der-capta.in>
2025-07-01 15:52:54 +0200

    Try to set fish as shell

--- home/common/base.nix
+++ home/common/base.nix
@@ -5,5 +5,28 @@
   home.homeDirectory = "/home/cozygalvinism";
   home.stateVersion = "25.05";
 
-  home.packages = with pkgs; [];
+  home.packages = with pkgs; [
+    fish
+  ];
+
+  programs.bash = {
+    interactiveShellInit = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+  programs.fish = {
+    plugins = [
+      { name = "plugin-git"; src = pkgs.fishPlugins.plugin-git.src; }
+      { name = "fzf-fish"; src = pkgs.fishPlugins.fzf-fish.src; }
+    ];
+  };
 }
