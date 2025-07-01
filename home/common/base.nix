58fd45a1f975f4847b410040c9a6ce9a905cb501
cozyGalvinism <jean@der-capta.in>
2025-07-01 15:56:59 +0200

    Set fish as login shell

--- home/common/base.nix
+++ home/common/base.nix
@@ -9,21 +9,12 @@
     fish
   ];
 
-  programs.bash = {
-    initExtra = ''
-      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-      then
-        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-      fi
-    '';
-  };
-
   programs.zoxide = {
     enable = true;
     enableFishIntegration = true;
   };
   programs.fish = {
+    enable = true;
     plugins = [
       { name = "plugin-git"; src = pkgs.fishPlugins.plugin-git.src; }
       { name = "fzf-fish"; src = pkgs.fishPlugins.fzf-fish.src; }
